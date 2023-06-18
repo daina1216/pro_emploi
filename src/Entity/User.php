93db1a3e5d88f3358793529b7bd4128dfde6dcf3
daina <tejeugnidaina@gmail.com>
2023-06-18 21:31:38 +0200

    inital commit

---
+++ src/Entity/User.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=45, nullable=true)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=25)
+     */
+    private $email_user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(?string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmailUser(): ?string
+    {
+        return $this->email_user;
+    }
+
+    public function setEmailUser(string $email_user): self
+    {
+        $this->email_user = $email_user;
+
+        return $this;
+    }
+}
